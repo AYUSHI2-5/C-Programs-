5b06bdaacbd36f66bcf03960d52e348dcad47b8e
Ayushi2-5 <ayushii.patel05@gmail.com>
2024-08-14 16:50:00 +0530

    Dynamic Programs

---
+++ 02_Dynamic_programs/p19_ Celsius_to_Fahrenheit.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+    float C;
+ printf("\n ~Celsius To Fehrenheit~");
+ printf("\n Celsius=> ");
+ scanf("%f",&C);
+ printf("\n Fehrenheit=> %f",(C*9/5)+32);
+ printf("\n");
+}
