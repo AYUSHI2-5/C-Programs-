c565e99b74d8e44769c241b727396c38b2c9a4af
Ayushi2-5 <ayushii.patel05@gmail.com>
2024-08-20 17:06:41 +0530

    Conditional

---
+++ 03_conditional_programs/p28_leapyear.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+ printf("\n ~Let's check Leap Year~");
+ int a;
+ printf("\n Enter Year=> ");
+ scanf("%d",&a);
+ if(a%4==0)
+ {
+ printf("\n It iS Leap Year");
+ }
+ else
+ {
+ printf("\n It is not Leap Year");
+ }
+}
