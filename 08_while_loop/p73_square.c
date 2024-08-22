3c27e58b805924b3f08f5273f0b04cac810e33cc
Ayushi2-5 <ayushii.patel05@gmail.com>
2024-08-22 12:57:41 +0530

    while loop

---
+++ 08_while_loop/p73_square.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+main()
+{
+ int i,no;
+ printf("\nEnter limit =>");
+ scanf("%d",&no);
+ i=1;
+ while(i<=no)
+ {
+ printf("\n%d ==== %d",i,i*i);
+ i++;
+ }
+}
---
+++ 08_while_loop/p78_digitreverse.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+main()
+{
+ int i,no1;
+ printf("\n Enter limit=> ");
+ scanf("%d",&no1);
+ i=no1;
+ while(i>=1){
+ printf(" %d",i);
+ i--;
+ }
+}
